34600c647cf24f8522dbd49f8046eb7e3108db27
John <john.sarmiento452@gmail.com>
2025-06-04 14:27:01 +0800

    create logi api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -12,6 +13,31 @@ export const signup = async (req, res, next) => {
     res
       .status(200)
       .json({ status: "success", message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+
+    if (!validuser) {
+      return next(errorHandler(404, "Invalid credentials. Please try again."));
+    }
+    const validPassword = await bcrypt.compare(password, validuser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid credentials. Please try again."));
+    }
+    const { password: pass, ...rest } = validuser._doc;
+
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
